88fcee1b801093c789a5a9235c3e7e21f1114022
HP <HP@DESKTOP-OGJNPOF>
2026-05-16 20:29:01 +0530

    enable swagger in production ds

--- Program.cs
+++ Program.cs
@@ -9,8 +9,11 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.UseSwaggerUI();
-app.UseSwaggerUI();
-
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Munavvar API V1");
+    c.RoutePrefix = "swagger";
+});
 app.UseHttpsRedirection();
 
 var summaries = new[]
